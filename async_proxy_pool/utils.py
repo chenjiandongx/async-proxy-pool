536695e7a59a3e5924c429bcfe89e0aee5a9cb83
chenjiandongx <chenjiandongx@qq.com>
2018-05-16 18:18:56 +0800

    Add: 新增延迟时间参数

--- async_proxy_pool/utils.py
+++ async_proxy_pool/utils.py
@@ -5,15 +5,16 @@ import asyncio
 
 import aiohttp
 
-from .config import HEADERS, REQUEST_TIMEOUT
+from .config import HEADERS, REQUEST_TIMEOUT, REQUEST_DELAY
 
 
-async def _get_page(url):
+async def _get_page(url, sleep):
     """
     获取并返回网页内容
     """
     async with aiohttp.ClientSession() as session:
         try:
+            await asyncio.sleep(sleep)
             async with session.get(
                 url, headers=HEADERS, timeout=REQUEST_TIMEOUT
             ) as resp:
@@ -22,13 +23,14 @@ async def _get_page(url):
             return ""
 
 
-def requests(url):
+def requests(url, sleep=REQUEST_DELAY):
     """
     请求方法，用于获取网页内容
 
     :param url: 请求链接
+    :param sleep: 延迟时间（秒）
     """
     loop = asyncio.get_event_loop()
-    html = loop.run_until_complete(asyncio.gather(_get_page(url)))
+    html = loop.run_until_complete(asyncio.gather(_get_page(url, sleep)))
     if html:
         return "".join(html)
