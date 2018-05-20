c86a8e0a46d074ab9e22a34a755e3b253e3def0d
chenjiandongx <chenjiandongx@qq.com>
2018-05-20 13:41:42 +0800

    Optimize: aiohttp 不支持 https 代理，导致测试结果不准确，现改用 requests 进行测试

--- test/test_proxy.py
+++ test/test_proxy.py
@@ -3,52 +3,53 @@
 
 import os
 import random
-import asyncio
+from concurrent.futures import ThreadPoolExecutor
 
-import aiohttp
+import requests
 
+
+HEADERS = {
+    "X-Requested-With": "XMLHttpRequest",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+}
 
 SUCCESS = 0
 FAIL = 0
 TIMEOUT = 15
 
-TEST_COUNT = int(os.environ.get("TEST_COUNT")) or 1000
-TEST_WEBSITE = os.environ.get("TEST_WEBSITE") or "https://httpbin.org/"
+TEST_COUNT = os.environ.get("TEST_COUNT") or 1000
+TEST_WEBSITE = os.environ.get("TEST_WEBSITE") or "https://zhihu.com"
 TEST_PROXIES = os.environ.get("TEST_PROXIES") or "http://localhost:3289/get/20"
 
 
-async def test_proxy(proxy, url):
+def get_proxies():
+    _proxies = requests.get(TEST_PROXIES, timeout=TIMEOUT).json()
+    return _proxies
+
+
+def test_one_proxy(proxy):
     global SUCCESS, FAIL
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(url, proxy=proxy, timeout=TIMEOUT) as resp:
-                if resp.status == 200:
-                    SUCCESS += 1
-                else:
-                    FAIL += 1
-        except:
+    try:
+        req = requests.get(
+            TEST_WEBSITE, proxies=proxy, timeout=TIMEOUT, headers=HEADERS
+        )
+        if req.status_code == 200:
+            SUCCESS += 1
+        else:
             FAIL += 1
-
-
-async def get_proxies():
-    async with aiohttp.ClientSession() as sess:
-        async with sess.get(TEST_PROXIES, timeout=TIMEOUT) as resp:
-            _proxies = []
-            for proxy in await resp.json():
-                for p in proxy.values():
-                    _proxies.append(p)
-            return _proxies
+    except:
+        FAIL += 1
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    proxies = loop.run_until_complete(get_proxies())
-    tasks = [
-        test_proxy(random.choice(proxies), TEST_WEBSITE)
-        for _ in range(TEST_COUNT)
-    ]
-    loop.run_until_complete(asyncio.wait(tasks))
+    proxies = get_proxies()
+    tasks = [random.choice(proxies) for _ in range(TEST_COUNT)]
+    with ThreadPoolExecutor(max_workers=64) as executor:
+        executor.map(test_one_proxy, tasks)
+    print("测试代理：", TEST_PROXIES)
     print("测试网站：", TEST_WEBSITE)
+    print("测试次数：", TEST_COUNT)
     print("成功次数：", SUCCESS)
     print("失败次数：", FAIL)
     print("成功率：", SUCCESS / TEST_COUNT)
